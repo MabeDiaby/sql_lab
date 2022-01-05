d2ab362a471e26a2ab5eb14154c1a15acde7ef6e
Mabe-Diaby <diabymabe@gmail.com>
2022-01-05 17:01:35 -0500

    question 4-12

--- NFL/nfl.sql
+++ NFL/nfl.sql
@@ -12,31 +12,39 @@ SELECT head_couch FROM teams WHERE conference = 'AFC' AND division = 'South';
 
 -- 4. The total number of players in the NFL
 
-
+SELECT COUNT(*) FROM players;
 
 -- 5. The team names and head coaches of the NFC North and AFC East
 
+SELECT name, head_couch FROM teams WHERE (conference = 'NFC' AND division = 'North') OR (conference = 'AFC' AND division = 'East');
 
 -- 6. The 50 players with the highest salaries
 
+SELECT * FROM players ORDER BY salary desc LIMIT 50;
 
 -- 7. The average salary of all NFL players
 
+SELECT AVG(salary) FROM players;
 
 -- 8. The names and positions of players with a salary above 10_000_000
 
+SELECT name, position FROM players WHERE salary > 10000000
 
 -- 9. The player with the highest salary in the NFL
 
+SELECT name FROM players WHERE salary = (select max(salary) from players)
 
 -- 10. The name and position of the first 100 players with the lowest salaries
 
+SELECT name, position FROM players ORDER BY salary;
 
 -- 11. The average salary for a DE in the nfl
 
+SELECT AVG(salary) from players WHERE position = 'DE'
 
 -- 12. The names of all the players on the Buffalo Bills
 
+SELECT player.name FROM teams JOIN players ON (teams.name = 'Buffalo Bills' )
 
 -- 13. The total salary of all players on the New York Giants
 
