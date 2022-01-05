2736ba31a4b71b6c6a2359d661ea17d38978397f
Mabe-Diaby <diabymabe@gmail.com>
2022-01-05 16:35:09 -0500

    nfl problems 1-3

--- NFL/nfl.sql
+++ NFL/nfl.sql
@@ -1,13 +1,17 @@
 -- 1. List the names of all NFL teams'
 
+SELECT name FROM teams;
 
 -- 2. List the stadium name and head coach of all NFC teams
 
+SELECT stadium, head_couch FROM teams WHERE conference = 'NFC';
 
 -- 3. List the head coaches of the AFC South
 
+SELECT head_couch FROM teams WHERE conference = 'AFC' AND division = 'South';
 
 -- 4. The total number of players in the NFL
+
 
 
 -- 5. The team names and head coaches of the NFC North and AFC East
